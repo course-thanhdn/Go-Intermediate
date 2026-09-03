d66765af0150399868c7f5e78cb65dbc7df7f979
ngocthanh06 <ngocthanh6398@gmail.com>
2026-09-03 23:08:52 +0700

    done

--- main.go
+++ main.go
@@ -5,31 +5,43 @@ import (
     "fmt"
     "os"
     "strconv"
+    "strings"
 )
 
-type Logger struct{}
-
-func (l Logger) Log(msg string) {
-    fmt.Printf("[log] %s\n", msg)
+type Stack struct {
+    items []int
 }
 
-type Counter struct {
-    Logger
-    count int
+func (s *Stack) Push(x int) {
+    s.items = append(s.items, x)
 }
 
-func (c *Counter) Inc() { 
-    c.count++
-    c.Logger.Log(strconv.Itoa(c.count))
+func (s *Stack) Pop() (int, bool) {
+    if len(s.items) == 0 {
+        return 0, false
+    }
+
+    lastIndex := len(s.items) - 1
+    value := s.items[lastIndex]
+
+    s.items = s.items[:lastIndex]
+
+    return value, true
 }
 
 func main() {
     sc := bufio.NewScanner(os.Stdin)
     sc.Scan()
-    n, _ := strconv.Atoi(sc.Text())
-    var c Counter
-    for i := 0; i < n; i++ {
-        c.Inc()
+    parts := strings.Fields(sc.Text())
+    var s Stack
+    for _, p := range parts {
+        n, _ := strconv.Atoi(p)
+        s.Push(n)
     }
-    _ = c
+    for {
+        x, ok := s.Pop()
+        if !ok { break }
+        fmt.Println(x)
+    }
+    // _ = fmt.Print
 }
