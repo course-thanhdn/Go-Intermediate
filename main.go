69f86b4db3fc0a9ccd1be1a2ebf63584f883258b
ngocthanh06 <ngocthanh6398@gmail.com>
2026-09-03 20:53:07 +0700

    done

--- main.go
+++ main.go
@@ -1,34 +1,22 @@
 package main
-import ("bufio"; "fmt"; "os"; "strconv"; "strings")
+import ("bufio"; "fmt"; "os"; "strconv")
+
+func safeDivide(a, b int) (q int, err error) {
+    if b == 0 {
+        return 0, fmt.Errorf("divide by zero")
+    }
+    
+    return a / b, nil
+}
+
 func main() {
     sc := bufio.NewScanner(os.Stdin)
-    sc.Scan()
-    fields := strings.Fields(sc.Text())
-    a := make(chan int)
-    b := make(chan int)
-    
-    go func() {
-        defer close(a)
-
-        for _, f := range fields {
-            n, _ := strconv.Atoi(f)
-
-            a <- n
-        }
-    }()
-
-    // go func() {
-    //     defer close(b)
-
-    //     for n := range a {
-    //         b <- n * n
-    //     }
-    // }()
-
-    // sum := 0
-    // for v := range b {
-    //     sum += v
-    // }
-
-    fmt.Println(sum)
+    sc.Scan(); a, _ := strconv.Atoi(sc.Text())
+    sc.Scan(); b, _ := strconv.Atoi(sc.Text())
+    q, err := safeDivide(a, b)
+    if err != nil {
+        fmt.Printf("error: %s\n", err)
+    } else {
+        fmt.Printf("result: %d\n", q)
+    }
 }
